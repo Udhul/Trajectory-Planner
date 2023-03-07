6f4170c3bc0581581166ba7d01f6b6761e7c17d8
Udhul <oldengloe@gmail.com>
2023-03-07 21:35:41 +0100

    Integrate executor call in window. run test

--- main.py
+++ main.py
@@ -7,9 +7,32 @@ import serial
 import serial.tools.list_ports
 from gcodeparser import GcodeParser
 
-# open gcode file and store contents as variable
-with open('./test/test.nc', 'r') as f:
-    gcode_parsed = GcodeParser(f.read())
+
+
+#__________________________________________________________________________________________________________
+#------ Definitions --------------------------------------------------------------------------------------->
+#¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
+#---Header General
+HEADER_FONT_TITLE       = 'verdana 16 bold'
+HEADER_FONT_SUBTITLE    = 'verdana 12 bold'
+HEADER_COLOR_BG         = 'yellow'
+HEADER_PAD              = (0,0)
+
+#---Footer General
+FOOTER_PAD              = (0,0)
+FOOTER_COLOR_BG         = 'black'
+
+#---Body General
+BODY_COLOR_BG           = 'grey'
+
+#---Start window
+SIZE_BLOCK_START        = (256,256)
+TITLE_START             = 'CNC Trajectory Planner'
+SUBTITLE_START          = 'Connect to Controller'
+ERROR_NO_PORT           = 'No port found...'
+COLOR_BUTTON_REFRESH    = 'teal'
+COLOR_BUTTON_CONNECT    = 'dark green'
+COLOR_BUTTON_EXIT       = 'dark red'
 
 
 
@@ -63,36 +86,6 @@ def gcode_executor(gcode_parsed, line_no):
     # Tool change commands. Placeholder. Currently unsupported
     if command_type == 'T':
         ... 
-
-for line in range(50):
-    gcode_executor(gcode_parsed, line)
-
-
-
-#__________________________________________________________________________________________________________
-#------ Definitions --------------------------------------------------------------------------------------->
-#¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
-#---Header General
-HEADER_FONT_TITLE       = 'verdana 16 bold'
-HEADER_FONT_SUBTITLE    = 'verdana 12 bold'
-HEADER_COLOR_BG         = 'yellow'
-HEADER_PAD              = (0,0)
-
-#---Footer General
-FOOTER_PAD              = (0,0)
-FOOTER_COLOR_BG         = 'black'
-
-#---Body General
-BODY_COLOR_BG           = 'grey'
-
-#---Start window
-SIZE_BLOCK_START        = (256,256)
-TITLE_START             = 'CNC Trajectory Planner'
-SUBTITLE_START          = 'Connect to Controller'
-ERROR_NO_PORT           = 'No port found...'
-COLOR_BUTTON_REFRESH    = 'teal'
-COLOR_BUTTON_CONNECT    = 'dark green'
-COLOR_BUTTON_EXIT       = 'dark red'
 
 
 
@@ -260,6 +253,8 @@ def window_main(ser=None):
                 [
                     sg.Button('Reconnect', key='main/reconnect'),
                     sg.Button('Settings', key='main/settings'),
+                    sg.Button('Load G-Code', key='main/load_gcode_button'),
+                    sg.Input('', key='load_gcode_input'),
                     sg.Button('Exit', expand_x=True, button_color=COLOR_BUTTON_EXIT),
                 ],
             ], element_justification='c', background_color=FOOTER_COLOR_BG, pad=FOOTER_PAD, expand_x=True)
@@ -287,6 +282,29 @@ def window_main(ser=None):
 
         if event == 'main/reconnect':
             ser = window_start(first_launch=False)
+
+        if event == 'main/load_gcode_button':
+            # Get the gcode path from the file browse popup
+            gcode_path = sg.popup_get_file(message='Load G-code', title='Load G-code', default_extension='.nc', keep_on_top=True)
+            # sg.FileBrowse(button_text='Select G-Code', target='load_gcode_input')
+
+            # Try parsing the gcode
+            try:
+                with open(gcode_path, 'r') as f:
+                    # Raw string form of the file
+                    gcode_loaded = f.read()
+                    # Parsed object format
+                    gcode_parsed = GcodeParser(gcode_loaded)
+            # If the gcode could not be parsed, print the error (maybe it is a wrong or unsupported file)
+            except Exception as e:
+                print(f'The chosen file could not be parsed as a G-Code. Error: {e}')
+
+            # Test run through the first 50 lines
+            for line in range(50):
+                gcode_executor(gcode_parsed, line)
+
+
+
 #__________________________________________________________________________________________________________
 #------ Main loop ----------------------------------------------------------------------------------------->
 #¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
